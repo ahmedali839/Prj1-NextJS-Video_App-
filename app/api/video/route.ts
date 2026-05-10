a1ed316c85001fc000a1fc6b8360cc37b3933752
Ahmed <ahmedyarr1212@gmail.com>
2026-05-10 16:30:05 +0500

    backend, api's endpoints

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,13 +1,27 @@
 import { connectToDatabase } from "@/lib/db";
-
+import Video from "@/models/video";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
-    try {
-        await connectToDatabase();
+  try {
+    await connectToDatabase();
 
-        Video
-    } catch (error) {
-        
+    const videos = await Video.find({}).sort({ createdAt: -1 }).lean();
+
+    if (!videos || videos.length === 0) {
+      return NextResponse.json([], { status: 200 });
     }
-    
+
+    return NextResponse.json(videos);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to fetch videos" },
+      { status: 500 },
+    );
+  }
+}
+
+
+export async function POST() {
+     
 }
