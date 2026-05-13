5edd05b69a3c694ea4ffc703234022db150ae1df
Ahmed <ahmedyarr1212@gmail.com>
2026-05-13 23:25:07 +0500

    backend:validations, imageKit config

--- lib/api-client.ts
+++ lib/api-client.ts
@@ -1,28 +1,45 @@
+import { IVideo } from "@/models/video";
+
+export type VideoFormData = Omit<IVideo, "_id">;
+
 type FetchOption = {
-    method?: "GET" || "POST" || "PUT" || "DELETE"
-    body? : any
-    headers? :Record<string, string>
-}
+  method?: "GET" | "POST" | "PUT" | "DELETE";
+  body?: any;
+  headers?: Record<string, string>;
+};
 
 class ApiClient {
-    private async fetch<T>(
-        endpoint: string,
-        options: FetchOption = {}
-    ) : Promise<T> {
-        const {method="GET", body, headers = {}} = options
+  private async fetch<T>(
+    endpoint: string,
+    options: FetchOption = {},
+  ): Promise<T> {
+    const { method = "GET", body, headers = {} } = options;
     const defaultHeaders = {
-        "Content-Type" : "application/json",
-        ...headers,
+      "Content-Type": "application/json",
+      ...headers,
+    };
+    const response = await fetch(`/api/${endpoint}`, {
+      method,
+      headers: defaultHeaders,
+      body: body ? JSON.stringify(body) : undefined,
+    });
+    if (!res.ok) {
+      throw new Error(await response.text());
     }
-const response = await fetch(`/api/${endpoint}` , {
-        method,
-        headers: defaultHeaders,
-        body: body ? JSON.stringify(body)  : undefined
-    })
-if(!res.ok) {
-throw new Error(await response.text())
-}
-    return response.json()
+    return response.json();
     // }
-    } 
+  }
+
+  async getVideos() {
+    return this.fetch("/videos");
+  }
+
+  async createVideos(videoData: VideoFormData) {
+    return this.fetch(`/videos`, {
+      method: "POST",
+      body: videoData,
+    });
+  }
 }
+
+export const apiClient = new ApiClient();
