7c91e99307b91c58cddfff0d2178c619a24cbb50
Ahmed <ahmedyarr1212@gmail.com>
2026-05-14 23:56:40 +0500

    fetching posts inside frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
+import { apiClient } from "../lib/api-client";
+
+async function getLocalVideos() {
+  const dataType = await apiClient.getVideos();
+  console.log(typeof dataType);
+}
 
 export default function Home() {
-
-  const dataType = JSON.parse(5);
-  console.log(typeof(dataType));
   
+  const data = getLocalVideos();
+  console.log(data);
+
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
