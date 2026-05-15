367106ef6494f727724c4fae7e566dbae556921f
Ahmed <ahmedyarr1212@gmail.com>
2026-05-15 22:08:54 +0500

    homepage

--- app/page.tsx
+++ app/page.tsx
@@ -34,14 +34,8 @@ export default function Home() {
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
               Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
+            </a>
+           
             center.
           </p>
         </div>
