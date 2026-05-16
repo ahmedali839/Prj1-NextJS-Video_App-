fae5944dbc91282629673062b0173dec39a83090
Ahmed <ahmedyarr1212@gmail.com>
2026-05-16 23:26:41 +0500

    homepage

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h2>let's build it in short time, Ahmed!!</h2>
-        <h3>Video</h3>
+        <h3>Video App</h3>
       </main>
     </div>
   );
